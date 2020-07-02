f47b8e0b9e5799aeff97cc9dd597b48ea7bb48da
Josue Carames <josue.carames@icloud.com>
2020-07-02 22:05:38 +0000

    format

--- canvas_db/Major Queries/Group.sql
+++ canvas_db/Major Queries/Group.sql
@@ -2,10 +2,10 @@ USE canvas;
 SELECT
     account_dim.name AS "Account Name"
     , enrollment_term_dim.name AS "Term Name"
-	, enrollment_term_dim.date_start AS "Term Start On"
-	, enrollment_term_dim.date_end AS "Term Ends On"
+    , enrollment_term_dim.date_start AS "Term Start On"
+    , enrollment_term_dim.date_end AS "Term Ends On"
     , group_dim.name AS "Group Name"
-	, group_dim.description AS "Group Description"
+    , group_dim.description AS "Group Description"
     , group_dim.created_at AS "Group Created At"
     , group_dim.updated_at AS "Group Updated At"
     , group_dim.deleted_at AS "Group Deleted At"
@@ -17,13 +17,13 @@ SELECT
     , group_dim.default_view AS "Group Default View"
     , group_dim.sis_source_id AS "Group SIS ID"
     , wiki_dim.created_at AS "Wiki Created At"
-	, wiki_dim.front_page_url AS "Wiki Front Page URL"
-	, wiki_dim.has_no_front_page AS "Wiki Has No Front Page"
-	, wiki_dim.parent_type AS "Wiki Parent Type"
-	, wiki_dim.title AS "Wiki Title"
-	, wiki_dim.updated_at AS "Wiki Updated At"
+    , wiki_dim.front_page_url AS "Wiki Front Page URL"
+    , wiki_dim.has_no_front_page AS "Wiki Has No Front Page"
+    , wiki_dim.parent_type AS "Wiki Parent Type"
+    , wiki_dim.title AS "Wiki Title"
+    , wiki_dim.updated_at AS "Wiki Updated At"
     , max_membership
-	, storage_quota
+    , storage_quota
 FROM group_fact
 INNER JOIN group_dim ON group_dim.id = group_fact.group_id
 -- INNER JOIN group_category_id ON group_category_dim.id = group_fact.group_category_id
